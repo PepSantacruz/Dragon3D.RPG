c333ebc4a835c082a0b2296d5062f2b8113f45cf
Pep Santacruz <santacruzpep@gmail.com>
2019-01-07 22:54:02 +0100

    Camera rotation

---
+++ Assets/ChangeCameraPoint.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace RPG.CameraUI {
+    public class ChangeCameraPoint : MonoBehaviour {
+        [SerializeField] float timeOfTravel = 2;
+
+        RectTransform rectTransformCamera;
+
+        void Start() {
+            rectTransformCamera = Camera.main.GetComponent<RectTransform>();
+        }
+
+        private void OnTriggerEnter(Collider other) {
+            // TODO use scriptable object to store the desired position
+            print("Enter camera change");
+            StartCoroutine(LerpObject(other.gameObject.transform,rectTransformCamera.anchoredPosition3D, new Vector3(0, 8, -10)));
+        }
+
+        IEnumerator LerpObject(Transform targetToLook, Vector3 startPosition, Vector3 endPosition) {
+            float currentTime = 0;
+            while (currentTime <= timeOfTravel) {
+                currentTime += Time.deltaTime;
+                float normalizedValue = currentTime / timeOfTravel; // we normalize our time 
+                rectTransformCamera.anchoredPosition3D = Vector3.Lerp(startPosition, endPosition, normalizedValue);
+                rectTransformCamera.LookAt(targetToLook);
+                yield return null;
+            }
+
+        }
+    }
+}
---
+++ Assets/_CameraUI/CameraChange.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace RPG.CameraUI {
+    public class CameraChange : MonoBehaviour {
+        public void ChangePosition(Vector3 moveToPosition) {
+            // Move the camera into position
+            transform.position = Vector3.Lerp(transform.position, moveToPosition, 2);
+
+            // Ensure the camera always looks at the player
+            //transform.LookAt(transform.parent);
+        }
+    }
+}
